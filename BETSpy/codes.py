54f6e6b39deba14d3b5424c144170acf0c557da2
Jonatha Azevedo da Costa <jonatha.costa@fgv.br>
2018-10-05 11:33:33 -0300

    build the functions

--- BETSpy/codes.py
+++ BETSpy/codes.py
@@ -4,12 +4,32 @@ def connection():
     return()
 
 
-def BETSget():
-    return()
-    
     
 def BETSsearch():
     return()
     
+def getSeriesBacen(x = None, strat = '', to = '',save = ''):
+    
+    if(x is None):
+        return('Need to specify at least one series.')
+    
+    return()
 
+
+def getSeries(code, start = '', to = '', df = False, frequency = None):
+    return()
+
+def BETSget(code, start = '', to = '', df = False, frequency = None):
+    n = len(code)
+    f = len(start)
+    t = len(to)
+    
+    if(n > 1):
+        ts = []
+        nms = []
+        
+        if(f == 1 and t == 1):
+            for i in range(n):
+              ts[i] =   getSeries()
+    return()
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BETSpy
+
+![GitHub repo size in bytes](https://img.shields.io/github/repo-size/GreedBlink/BETSpy.svg)
+
+
+A python version of BETS (Brazilian Economic Time Series) package. 
